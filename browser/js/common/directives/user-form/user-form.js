5031951ceaef6d5730ff006e34c15e2f48e60dd3
rbtkoz <kozovski.a@gmail.com>
2015-12-16 16:39:12 -0500

    first commit

---
+++ browser/js/common/directives/user-form/user-form.js
@@ -0,0 +1,9 @@
+/**
+ * Created by alex.kozovski on 12/16/15.
+ */
+app.directive('userForm', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'js/common/directives/user-form/user-form.html'
+    };
+});
