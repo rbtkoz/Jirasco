a2e17180b8d0f9872f909f8ea396e66cc9aae075
Alexander Kozovski <kozovski.a@gmail.com>
2016-01-08 13:18:54 -0500

    respond to slack

--- server/app/configure/custom.js
+++ server/app/configure/custom.js
@@ -6,14 +6,16 @@
 var mongoose = require('mongoose');
 var RecoModel = mongoose.model('Reco');
 var JiraApi = require('jira').JiraApi;
+var request = require('request');
 
 module.exports = function(app){
 
-
+    var slack_url;
+    var slack_user;
     var JiraConfig = app.getValue('env').JIRA;
     var jira = new JiraApi('https', JiraConfig.host,JiraConfig.port,JiraConfig.user, JiraConfig.password, '2');
 
-    app.post("/update", function(req,res,next){
+    app.post("/update", function(req,res,next) {
 
         //find issue code
         //jira.findIssue('NEW-1', function(error, issue) {
@@ -25,55 +27,70 @@ module.exports = function(app){
         //add new issue
         var issue = req.body.params;
         var payload =
-            {
-                "fields": {
-                "project":
-                {
+        {
+            "fields": {
+                "project": {
                     "id": "10000"
                 },
-                    "summary": issue.name,
-                    "description": issue.description,
-                    "customfield_10026":issue.email,
-                    "customfield_10027":issue.origin,
-                    "customfield_10025": issue.subject,
-                    "customfield_10024": issue.products,
-                    "priority": {
-                        "name":issue.priority
-                    },
-                    "issuetype": {
-                        "name": "Task"
-                    }
+                "summary": issue.name,
+                "description": issue.description,
+                "customfield_10026": issue.email,
+                "customfield_10027": issue.origin,
+                "customfield_10025": issue.subject,
+                "customfield_10024": issue.products,
+                "priority": {
+                    "name": issue.priority
+                },
+                "issuetype": {
+                    "name": "Task"
                 }
             }
+        }
 
         //for mongodb
         var mongopayload =
-            {
-                summary: issue.name,
-                description: issue.description,
-                initiator: issue.email,
-                customer: issue.origin,
-                subject: issue.subject,
-                products: issue.products,
-                priority: issue.priority
-            }
+        {
+            summary: issue.name,
+            description: issue.description,
+            initiator: issue.email,
+            customer: issue.origin,
+            subject: issue.subject,
+            products: issue.products,
+            priority: issue.priority
+        }
 
-        jira.addNewIssue(payload, function(err, resp) {
+        jira.addNewIssue(payload, function (err, resp) {
 
-            if(err) {
+            if (err) {
                 console.log(err.errors, "err")
                 return res.json(err);
-            } else{
+            } else {
                 console.log(resp, "...also Saving to DB")
 
-                RecoModel.create(mongopayload, function(err, post){
-                    if(err) return next(err);
-                    //res.send('https://hooks.slack.com/commands/1234/5678',{
-                    //    text: 'Thanks for Improving Macmillan Learning'
-                    //});
-                })
-                //return res.json(resp);
-            }
+                RecoModel.create(mongopayload, function (err, post) {
+                    if (err) {
+                        return next(err);
+
+                    } else {
+
+                        var payload_slack ={"text":"Thanks "+slack_user+", you rock!"};
+                        var options = {
+                            url:     slack_url,
+                            method: "POST",
+                            json:true,
+                            body:payload_slack
+                        };
+
+                        request.post(options, function(error, response, body){
+                            if(!err){
+                                return res.json(resp);
+                            }
+                        });
+
+
+                    }
+                });
+            };
         });
     });
 
@@ -82,34 +99,31 @@ module.exports = function(app){
         console.log(req.body, "request body");
 
         var response = {
-            "attachments": [
-            {
-                "fallback": "Click to start improving! - https://jirasco-app.herokuapp.com/",
-                "pretext": "Made by MLDPI",
-                "title": "Click to start improving our products!",
-                "title_link": "https://jirasco-app.herokuapp.com/",
-                "color": "#7CD197"
-            }
-            ]
-        }
-
-
-
-
-        res.send({
             text: 'Created by ML+DPI',
             attachments: [
                 {
-                    "title": "Click to start improving our products!",
-                    "title_link": "https://jirasco-app.herokuapp.com/"
+                    "title": "Hi "+req.body.user_name + ", Click to start improving our products!",
+                    "title_link": "https://jjgthzbzsx.localtunnel.me"
+
                 }
             ]
-        });
+        };
+
+        if(req.body.token === "UbroR1OQk5B1OeOswN3G7N5H"){
+            res.send(response);
+
+            slack_url = req.body.response_url;
+            slack_user =req.body.user_name;
+        }
     });
 
 
+    //app.post("/toslack", function(req, res, next) {
+    //});
+
 }
 
+//JIRA CURL DONOTREMOVE
 //https://mlexperiments.atlassian.net/rest/api/latest/issue/NEW-1.json
 //curl -D- -u "admin:1NYP48" -X GET -H "Content-Type: application/json"  https://mlexperiment.atlassian.net/rest/api/2/issue/MFP
 //curl -D- -u "admin:1NYP48" -X GET -H "Content-Type: application/json"  https://mlexperiment.atlassian.net/rest/api/2/project
