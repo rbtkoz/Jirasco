753e466949af34fd08e80fba8b111f9e47490f96
rbtkoz <kozovski.a@gmail.com>
2016-06-23 16:12:03 -0400

    eliminate Angular

---
+++ browser/js/common/directives/jira_script/jira.js
@@ -0,0 +1,51 @@
+/**
+ * Created by alex.kozovski on 6/23/16.
+ */
+app.directive('jira', function ($rootScope, AuthService, AUTH_EVENTS, $state) {
+
+    return {
+        restrict: 'E',
+        scope: {},
+        templateUrl: 'js/common/directives/jira_script/jira.html',
+        link: function (scope) {
+
+            scope.items = [
+                { label: 'Home', state: 'home' },
+                { label: 'About', state: 'about' },
+                { label: 'Documentation', state: 'docs' },
+                { label: 'Members Only', state: 'membersOnly', auth: true }
+            ];
+
+            scope.user = null;
+
+            scope.isLoggedIn = function () {
+                return AuthService.isAuthenticated();
+            };
+
+            scope.logout = function () {
+                AuthService.logout().then(function () {
+                    $state.go('home');
+                });
+            };
+
+            var setUser = function () {
+                AuthService.getLoggedInUser().then(function (user) {
+                    scope.user = user;
+                });
+            };
+
+            var removeUser = function () {
+                scope.user = null;
+            };
+
+            setUser();
+
+            $rootScope.$on(AUTH_EVENTS.loginSuccess, setUser);
+            $rootScope.$on(AUTH_EVENTS.logoutSuccess, removeUser);
+            $rootScope.$on(AUTH_EVENTS.sessionTimeout, removeUser);
+
+        }
+
+    };
+
+});
